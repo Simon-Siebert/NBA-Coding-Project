ce7cb1924ed3d335f382a0a70ee34fca53001570
Simon Siebert <simonsiebert15@gmail.com>
2023-10-29 23:01:42 -0500

    Loaded CSV's

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import numpy as np
+
+# Use Pandas to read CSV's
+team_csv = pd.read_csv('All NBA Team Stats 2022-2023 CSV.csv')
+player_csv = pd.read_csv('All NBA Players 2022-2023 CSV.csv')
+# Print first five variables to observe data
+#print(team_csv.head(5))
+#print(player_csv.head(5))
